becfebcd65c528017672de331a8ffecaf52634af
Valerii Fazli <valerii.f@orot-technologies.co.il>
2015-08-27 00:04:53 +0300

    homepage update

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@
   var app = {};
   app = {
     init: function(){
-      $('.slide').height(window.innerHeight);
+      $('.slide, .slide-item').height(window.innerHeight);
       this.resizeSlide();
       this.createSlidesNav();
+      this.newsListItemWidth(4);
       this.bind();
       this.clickSlideNav();
+      this.scrollSwitchSlideNav();
     },
     bind: function(){
       var me = this;
@@ -18,10 +20,26 @@
         app.hideAuth();
         return false;
       });
+      $(".main_carousel, .solutions_carousel").owlCarousel({
+        loop:false,
+        margin:0,
+        nav:false,
+        items: 1
+      });
+      $('.new-list-wrapper').jScrollPane({
+        autoReinitialise: true
+      });
+      $(window).resize(function(){
+        var t;
+        clearTimeout(t);
+        t = setTimeout(function(){
+          app.newsListItemWidth(4);
+        }, 400);
+      });
     },
     resizeSlide : function(){
       $(window).resize(function(){
-        $('.slide').height(window.innerHeight);
+        $('.slide, .slide-item').height(window.innerHeight);
       });
     },
     createSlidesNav: function(){
@@ -31,7 +49,13 @@
         $(div).data('target', '#'+$('.slide').eq(i).attr('id'));
         $('.slides_nav').prepend(div);
       };
-      $('.slides_nav div').eq(0).addClass('active');
+      var scrolltop = $(document).scrollTop();
+      $('.slide').each(function(i, e){
+        if (scrolltop >= ($(this).position().top - 300) && ($(this).position().top + $(this).height() + 300) >= scrolltop ){
+          $('.slides_nav div').removeClass('active');
+          $('.slides_nav div').eq(i).addClass('active');
+        }
+      });;
     },
     scrollToSlide: function($element){
       $('html, body').animate({
@@ -44,6 +68,23 @@
         $(this).addClass('active');
         app.scrollToSlide($($(this).data('target')));
       });
+    },
+    scrollSwitchSlideNav: function(){
+      $(document).scroll(function(){
+        var scrolltop = $(this).scrollTop();
+        $('.slide').each(function(i, e){
+          if (scrolltop >= ($(this).position().top - 300) && ($(this).position().top + $(this).height() + 300) >= scrolltop ){
+            $('.slides_nav div').removeClass('active');
+            $('.slides_nav div').eq(i).addClass('active');
+          }
+        });
+      });
+    },
+    newsListItemWidth: function(visibleItems){
+      var d = visibleItems ? visibleItems : 4;
+      var w = $('.new-list-wrapper').width()/d;
+      $('.news-list li').width( w-40 );
+      $('.news-list').width($('.news-list li').length*w);
     },
     showAuth: function(){
       if($('.auth_wrapper').hasClass('show')){
